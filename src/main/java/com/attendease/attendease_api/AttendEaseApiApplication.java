05fa970f15d6720f9d2c7a5ecaeed9343c091c29
parag parmar <paragp022@gmail.com>
2025-08-14 20:41:15 +0530

    BE-01: Initial Spring Boot project setup

---
+++ src/main/java/com/attendease/attendease_api/AttendEaseApiApplication.java
@@ -0,0 +1,13 @@
+package com.attendease.attendease_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendEaseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendEaseApiApplication.class, args);
+	}
+
+}
